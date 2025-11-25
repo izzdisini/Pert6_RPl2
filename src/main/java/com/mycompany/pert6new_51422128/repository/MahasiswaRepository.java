88e70d9ee901e0b1e76eaba2e814248c2e98f508
unknown <raisfiqri1225@example.com>
2025-11-25 14:56:12 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert6new_51422128/repository/MahasiswaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert6new_51422128.repository;
+
+import com.mycompany.pert6new_51422128.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author rais fiqri
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+    
+
+}
+
